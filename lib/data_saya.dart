6bb75c6b85d9020a6fcade699e9f1b00620f76f7
msyar-i <msyari968@gmail.com>
2025-05-31 20:03:15 +0700

    uas selesai

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,4 @@
 class DataSaya {
-   static String nama = "Kudut";
-    static String gambar = "assets/mahasiswa/kudut.png";
+  static String nama = "Muhammad Syar'i";
+  static String gambar = "assets/syari.jpg";
 }
